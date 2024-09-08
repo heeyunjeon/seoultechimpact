fe0f8a1a520a2513c69767dcc3e2574c39f524e9
최슬기 <choisk09@gmail.com>
2024-09-08 10:27:38 +0900

    amended on the detail page

--- src/app/map/detail/page.js
+++ src/app/map/detail/page.js
@@ -5,14 +5,14 @@ import styles from "./page.module.css"
 import { IoIosArrowBack } from "react-icons/io";
 import { useRouter } from 'next/navigation';
 
-function detail() {
+function Detail() {
     const router = useRouter();
     const handleButtonClick = () => {
         router.push(`/map?object=${object}`);
 
     };
 
-    const object = 'toilet'
+    const object = 'trash'
 
     return <>
         <div className={styles.headerWrapper}>
@@ -20,16 +20,16 @@ function detail() {
                 size={30}
                 onClick={() => handleButtonClick(object)}
             />
-            <header className={styles.title}>숭인공공화장실</header>
+            <header className={styles.title}>Wonji-Dong - Bin 🗑</header>
             <div className={styles.imgWrapper}>
                 <img className={styles.image} src="https://upload.wikimedia.org/wikipedia/ko/thumb/8/83/Urbeach-washroom-changeroom-architecture.jpg/400px-Urbeach-washroom-changeroom-architecture.jpg" />
             </div>
             <div className={styles.informationCard}>
-                <p>📍 <strong>Location</strong><br /> &nbsp; - 서울시 종로구 숭인동</p><br />
-                <p>⏰ <strong>Available Time</strong><br /> &nbsp; - 06:00 - 24:00</p>
+                <p>📍 <strong>Location</strong><br /> &nbsp; - 594-10, Wonji-dong, Seocho-gu, Seoul</p><br />
+                <p>⏰ <strong>Trash</strong><br /> &nbsp; - Trash: Recyclables ♻️</p>
             </div>
         </div>
     </>;
 }
 
-export default detail;
+export default Detail;
